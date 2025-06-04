73ac23d8311fd0a609b902a5610fc732f8424e86
Emanuellacgomes <168464880+Emanuellacgomes@users.noreply.github.com>
2025-06-04 17:23:53 -0300

    Add

--- MobileProject/App.js
+++ MobileProject/App.js
@@ -3,12 +3,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import LoginUsu from './telas/LoginUsu';
+import CreateConsultationScreen from './telas/CriarConsultaTESTE';
 
 export default function App() {
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name='CriarConsulta' options={{headerShown:false}} component={CreateConsultationScreen}></Stack.Screen>
         <Stack.Screen name='Login' options={{headerShown:false}} component={LoginUsu}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
---
+++ MobileProject/index.js
@@ -0,0 +1,6 @@
+// index.js
+import { registerRootComponent } from 'expo';
+import App from './App'; // Importa seu componente App.js
+
+// Registra o componente raiz do seu aplicativo
+registerRootComponent(App);
